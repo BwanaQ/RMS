06a89b88792fb3fca0984e4e9f6294c21063104a
laurachebet1997 <rosechebet19971997@gmail.com>
2021-01-15 08:40:07 +0300

    remove spaces let

--- js/script2.js
+++ js/script2.js
@@ -2,15 +2,12 @@ var jq =$.noConflict();
 jq(document).ready(function() {
   jq("form#add-building").submit(function(event) {
     event.preventDefault();
-    //capture values from form
+    
     var inputtedBuildingID= jq("input#buildingID").val();
     var inputtedBuildingName = jq("input#buildingName").val();
   
-    //logic from inputs
+  
 
-    
-
-    //output
     var building="<tr>"
     + "<td>" + inputtedBuildingID +"</td>"+
     "<td>" + inputtedBuildingName +"</td>"+
